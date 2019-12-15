e8f20dac3586980a664114b530d2b88d6113ef30
Nathan Swanson <nathanswanson370@gmail.com>
2019-12-14 18:12:09 -0600

    reorganization

--- src/main/java/com/stthomas/swan9854/gui/GuiPlanner.java
+++ src/main/java/com/stthomas/swan9854/gui/GuiPlanner.java
@@ -7,22 +7,46 @@ import java.util.Set;
 
 import org.apache.commons.lang3.NotImplementedException;
 
+import com.stthomas.swan9854.core.RecipeCache;
 import com.stthomas.swan9854.craftable.Duple;
+import com.stthomas.swan9854.filter.OreDictionaryFilter;
+import com.stthomas.swan9854.gui.part.GuiLine;
+import com.stthomas.swan9854.gui.part.GuiPart;
+import com.stthomas.swan9854.gui.part.GuiRect;
+import com.stthomas.swan9854.gui.part.GuiString;
+import com.stthomas.swan9854.gui.part.IGuiPart;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class GuiPlanner {
 	
 	private ArrayList<IGuiPart> parts;
 	private static GuiPlanner instance;
-		
-	public static void initialize()
+	private HashMap<Duple, Integer> h;
+	private HashMap<String, Integer> stackOverload;
+	private HashMap<ItemStack, Integer> rawCount;
+	
+	private boolean ready;
+	
+	public static void initialize(ItemStack stack, int count)
 	{
 		instance = new GuiPlanner();
+		instance.ready = false;
+		instance.rawCount = new HashMap<ItemStack, Integer>();
 		instance.parts = new ArrayList<IGuiPart>();
-
+		instance.stackOverload = new HashMap<String, Integer>();
+		instance.h = new HashMap<Duple, Integer>();
+		
+		for(int i = 0; i < count; i++)
+			instance.recursiveRecipes(stack, 0);
+		
+		System.out.print(instance.rawCount);
+		instance.convertToGui(instance.h);
 	}
 	
 	public static GuiPlanner instance()
@@ -47,7 +71,7 @@ public class GuiPlanner {
 		return instance != null;
 	}
 	
-	public void convertToGui(HashMap<Duple, Integer> h) {
+	private void convertToGui(HashMap<Duple, Integer> h) {
 		// TODO Auto-generated method stub
 		Set<Duple> dupleSet = h.keySet();
 		int max = 0; 
@@ -59,11 +83,10 @@ public class GuiPlanner {
 			}
 		}
 		
-		Minecraft.getMinecraft().player.sendChatMessage(""+max);
 		
 		for(int i = 0; i <= max; i++)
 		{
-			parts.add(new GuiString("Phase: " + (max - i + 1)));
+			parts.add(new GuiString("Phase: " + (max - i + 1), "FFAA00"));
 			parts.add(new GuiLine(5,100,"ffffff"));
 			
 			GuiPart section = new GuiPart();
@@ -76,9 +99,155 @@ public class GuiPlanner {
 				}
 			}
 			parts.add(section);
-			parts.add(new GuiString(""));
+			parts.add(new GuiString("","ffffff"));
 		}
 		
+		parts.add(new GuiString("Materials","ff0000"));
+		parts.add(new GuiLine(5,100, "ffffff"));
+		GuiPart raw = new GuiPart();
+		Set<ItemStack> keys = rawCount.keySet();
+		for(ItemStack sets: keys)
+		{
+			Duple itemAndCount = new Duple(sets, rawCount.get(sets));
+			raw.add(itemAndCount);
+		}
+		parts.add(raw);
+		instance.ready = true;
 	}
 	
+	private void recursiveRecipes(ItemStack itemStack, int index)
+	{
+
+		IRecipe recipe = grabRecipe(itemStack);
+		if(itemStack != null & recipe != null)
+		{
+			if(recipe.getRecipeOutput().getDisplayName().equals(itemStack.getDisplayName()))
+	    	{
+
+				ItemStack[] stacks = new ItemStack[9];
+	    		for(int l = 0; l < recipe.getIngredients().size(); l++)
+	    		{
+	    			if(recipe.getIngredients().get(l).getMatchingStacks().length > 0)
+	    				stacks[l] = recipe.getIngredients().get(l).getMatchingStacks()[0];
+	    		}
+				if(stackNeedsFill(recipe, itemStack))
+				{
+					for(int i = 0; i < 9;i++)
+					{
+
+						if(itemStack.getMaxItemUseDuration() != 0)
+							break;
+						
+						if(OreDictionary.getOreIDs(itemStack).length > 0 && new OreDictionaryFilter().accept(itemStack))
+							break;
+						
+					    if(itemIsRecursive(itemStack))
+					    	break;
+					
+						instance.recursiveRecipes(stacks[i], index+1);
+					}
+	    		}
+	    	}
+		}
+		
+		if(itemStack != null)
+		{
+			Duple input = new Duple(itemStack, index);
+			
+			if(itemStack != null)
+			{
+				if(recipe == null || itemIsRecursive(itemStack))
+					rawCount.put(itemStack,  rawCount.get(itemStack) == null ? 1 : rawCount.get(itemStack) + 1);
+				
+				if(h.containsKey(input))
+				{
+					h.put(input, h.get(input) + 1);
+				}
+				else
+				{
+					h.put(input, 1);
+				}
+			}
+		}	
+	}
+	
+	private IRecipe grabRecipe(ItemStack itemStack) {
+		IRecipe[] recipes = RecipeCache.instance().matchListRecipes();
+		if(itemStack != null)
+		{
+
+			for(IRecipe recipe: recipes)
+			{
+				if(recipe.getRecipeOutput().getDisplayName().equals(itemStack.getDisplayName()))				{
+					return recipe;
+				}
+			}
+		}
+		return null;
+	}
+	
+	private boolean itemIsRecursive(ItemStack itemStack) {
+		IRecipe recipe = grabRecipe(itemStack);
+		if(itemStack != null & recipe != null)
+		{
+			if(recipe.getIngredients().size() > 0)
+			{
+				for(ItemStack stack: recipe.getIngredients().get(0).getMatchingStacks())
+				{
+					IRecipe recipe2 = grabRecipe(stack);
+					if(recipe2 != null && recipe2.getIngredients().size() > 0)
+					{
+						for(ItemStack stack2: recipe2.getIngredients().get(0).getMatchingStacks())
+						{
+							if(itemStack.getDisplayName().equals(stack2.getDisplayName()))
+							{
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}	
+		return false;
+	}
+
+	private boolean stackNeedsFill(IRecipe recipe, ItemStack itemStack) {
+		// if it does not exist yet it needs to be filled and created
+		if(!stackOverload.containsKey(itemStack.getDisplayName()))
+		{
+			stackOverload.put(itemStack.getDisplayName(), recipe.getRecipeOutput().getCount() - 1);
+			return true;
+		}
+		else if(stackOverload.get(itemStack.getDisplayName()) == 0)
+		{
+			stackOverload.put(itemStack.getDisplayName(), recipe.getRecipeOutput().getCount() - 1);
+			return true;
+		}
+		else
+		{
+			stackOverload.put(itemStack.getDisplayName(), stackOverload.get(itemStack.getDisplayName()) - 1);
+			return false;
+		}
+	}
+	
+	public boolean isReady()
+	{
+		return ready;
+	}
+
+	public static void updateInventory() {
+		// TODO Auto-generated method stub
+		if(instance.rawCount != null)
+		{
+		Set<ItemStack> sets = instance.rawCount.keySet();
+			InventoryPlayer inv = Minecraft.getMinecraft().player.inventory;
+			for(ItemStack stacks: sets)
+			{
+				if(inv.hasItemStack(stacks))
+				{
+					instance.rawCount.put(stacks,instance.rawCount.get(stacks) - inv.getStackInSlot(inv.getSlotFor(stacks)).getCount());
+				}
+			}
+		}
+	}
 }
--- src/main/java/com/stthomas/swan9854/gui/IGuiPart.java
+++
@@ -1,5 +0,0 @@
-package com.stthomas.swan9854.gui;
-
-public interface IGuiPart {
-	public Object getPart();
-}
